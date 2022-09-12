8ea77410514a589fec936f80138f83c992f45971
mario <manriqueduran123@gmail.com>
2022-09-12 15:36:26 -0500

    finished styling for now

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 const cardsContainer = document.querySelector(".cards-container");
+const p = document.createElement("p");
+p.classList.add("card-txt");
 const btnCard = document.querySelector(".btn-card");
 const addCard = document.querySelector(".add-card");
 
@@ -7,7 +9,11 @@ const addCard = document.querySelector(".add-card");
 function appendDiv() {
     let childrenDiv = cardsContainer.children.length;
     if (childrenDiv < 8) {
+        const p = document.createElement("p");
+        p.classList.add("card-txt");
+        p.textContent = "Empty";
         const newCard = document.createElement("div");
+        newCard.appendChild(p);
         newCard.classList.add("card");
         btnCard.insertAdjacentElement( "beforebegin", newCard);
     } else {
